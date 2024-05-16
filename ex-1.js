b97fd660c1dfbd2aedcdc171c2ab670275807572
wiKanok <kanokwan1401.ks@gmail.com>
2024-05-16 13:04:04 +0700

    feat:Loop ex1-6

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,13 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
+// console.log(scores);
 
 // Start coding here
+let newScores = [];
+
+for (let score of scores) {
+  score -= 10;
+  newScores.push(score);
+}
+
+console.log(newScores);
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,10 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore;
+let minScore = scores[0]; // กำหนด value minScore ให้เป็น scores Index ที่ 0 เพื่อใช้เปรียบค่ากับ ตัวอื่นๆใน array ค่ะ
 // Start coding here
-
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] < minScore) {
+    minScore = scores[i];
+  }
+}
 console.log(minScore);
